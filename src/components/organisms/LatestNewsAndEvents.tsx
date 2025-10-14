0d4503b79fa9e9769d177ebd47a35c356d42050f
Folacodes <fola4ril@gmail.com>
2025-10-14 11:13:30 +0100

    finished latest news and events section

--- src/components/organisms/LatestNewsAndEvents.tsx
+++ src/components/organisms/LatestNewsAndEvents.tsx
@@ -1,6 +1,6 @@
 import { newsAndEvents } from "@/data";
 import { HeaderText } from "../atoms/HeaderText";
-import { Calendar } from "lucide-react";
+import { Calendar, MapPin } from "lucide-react";
 
 const LatestNewsAndEvents = () => {
   return (
@@ -10,7 +10,7 @@ const LatestNewsAndEvents = () => {
         subtitle="Stay updated with our recent activities and upcoming initiatives."
       />
 
-      <div className="flex items-center justify-center gap-x-6 mt-16">
+      {/* <div className="flex items-center justify-center gap-x-6 mt-16">
         {newsAndEvents?.map(({ date, id, location, summary, tag, title }) => {
           return (
             <div
@@ -22,8 +22,52 @@ const LatestNewsAndEvents = () => {
                 <p className="font-semibold text-sm">{date}</p>
               </div>
 
-              <div className="mt-4 bg-secondary/10 py-1 px-3">
+              <div className="mt-4 bg-secondary/10 py-1 px-3 rounded-full">
                 <p className="text-secondary font-semibold text-xs">{tag}</p>
+              </div>
+
+              <p className="playfair font-semibold py-2 text-secondary text-xl">
+                {title}
+              </p>
+              <p className="font-normal text-green_200 text-base pb-6">
+                {summary}
+              </p>
+
+              <div className="flex items-center text-green_200">
+                <MapPin />
+                <p className="text-sm font-normal">{location}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div> */}
+
+      <div className="flex items-stretch justify-center gap-x-6 mt-16">
+        {newsAndEvents?.map(({ date, id, location, summary, tag, title }) => {
+          return (
+            <div
+              key={id}
+              className="drop-shadow-card p-6 bg-white border border-gray_200 rounded-lg w-[413px] flex flex-col"
+            >
+              <div className="flex items-center text-yellow_100">
+                <Calendar className="w-4 h-4 mr-2" />
+                <p className="font-semibold text-sm">{date}</p>
+              </div>
+
+              <div className="mt-4 bg-secondary/10 py-1 px-3 rounded-full w-fit">
+                <p className="text-secondary font-semibold text-xs">{tag}</p>
+              </div>
+
+              <p className="playfair font-semibold py-2 text-secondary text-xl">
+                {title}
+              </p>
+              <p className="font-normal text-green_200 text-base pb-6 flex-1">
+                {summary}
+              </p>
+
+              <div className="flex items-center text-green_200">
+                <MapPin />
+                <p className="text-sm font-normal">{location}</p>
               </div>
             </div>
           );
