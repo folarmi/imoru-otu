6280874dd4f786b401bea67b926b212a1a73dd32
Folacodes <fola4ril@gmail.com>
2025-12-05 14:14:51 +0100

    added meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,44 @@ import { RoutePage } from "./utils/RoutePage";
 import QueryClientContextProvider from "./lib/QueryClientContextProvider";
 import StoreProvider from "./lib/StoreProvider";
 import { Bounce, ToastContainer } from "react-toastify";
+import { Helmet } from "react-helmet-async";
 // import "react-tooltip/dist/react-tooltip.css";
 
 function App() {
   return (
-    <StoreProvider>
-      <QueryClientContextProvider>
-        {/* <AuthProvider> */}
-        {/* <Theme> */}
-        <Router>
-          <RoutePage />
-        </Router>
-        <ToastContainer
-          position="top-center"
-          pauseOnHover
-          hideProgressBar
-          transition={Bounce}
-          closeButton={false}
-          closeOnClick
-          autoClose={5000}
-        />
-      </QueryClientContextProvider>
-    </StoreProvider>
+    <Helmet>
+      <title>Imoru Otu Atuluse</title>
+
+      <meta
+        name="description"
+        content="A non-profit, non-political organization dedicated to community development,
+cultural preservation, and social welfare support since 2019."
+      />
+
+      <meta
+        name="keywords"
+        content="Imoru Otu Atuluse, imoru, Ondo, NGO, web developer"
+      />
+      <StoreProvider>
+        <QueryClientContextProvider>
+          {/* <AuthProvider> */}
+          {/* <Theme> */}
+          <Router>
+            <RoutePage />
+          </Router>
+          <ToastContainer
+            position="top-center"
+            pauseOnHover
+            hideProgressBar
+            transition={Bounce}
+            closeButton={false}
+            closeOnClick
+            autoClose={5000}
+          />
+        </QueryClientContextProvider>
+      </StoreProvider>
+      <link rel="canonical" href="https://imoruotuatuluse.com.ng" />
+    </Helmet>
   );
 }
 
